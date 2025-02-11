8de0a3bccbd0bd9f8ceb39618612100c856c6738
anderscheng326 <anderscheng326@gmail.com>
2025-02-11 14:34:01 -0500

    created reserveBlock

--- Lab3a_Appointments/Program.cs
+++ Lab3a_Appointments/Program.cs
@@ -5,6 +5,12 @@ public class Appointments {
     public Appointments() {
         timeSlots = new int[8, 60];
     }
+public void reserveBlock(int period, int startMinute, int duration) {
+    for (int x = 0; x < duration; x++) {
+        timeSlots[period=1,startMinute+x] = 1;
+    }
+}
+
 }
 
 
